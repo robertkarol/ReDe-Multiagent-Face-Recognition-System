d573204828bcae2eede18ca122a7eacaa3e01884
Robert Szarvas <robisz98@gmail.com>
2020-02-20 12:52:54 +0200

    test file update

--- Source/FaceRecognition/test_model.py
+++ Source/FaceRecognition/test_model.py
@@ -1,6 +1,9 @@
 from RecognitionModel import RecognitionModel
 
 model = RecognitionModel(normalize=True, encode_labels=False)
-model.load_data_from_compressed('5-celebrity-faces-embeddings2.npz', is_embeddings_file=True)
+model.load_data_from_compressed('locals/5-celebrity-faces-embeddings2.npz', is_embeddings_file=True)
 model.train()
+print(model.test() * 100)
+RecognitionModel.save_model_as_binary(model, "model-pickle")
+model = RecognitionModel.load_model_from_binary("model-pickle")
 print(model.test() * 100)
