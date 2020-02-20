c8be282b4cabe079b7cbfc6b3a01e411d1eeb6c8
Robert Szarvas <robisz98@gmail.com>
2020-02-20 12:35:32 +0200

    factorize code into helper

--- Source/FaceRecognition/DatasetHelpers.py
+++ Source/FaceRecognition/DatasetHelpers.py
@@ -7,14 +7,11 @@ from numpy import asarray
 
 class DatasetHelpers:
     @staticmethod
-    def extract_faces_from_image(path_to_image, required_size=(160, 160), single_face = False):
-        image = Image.open(path_to_image)
-        image = image.convert('RGB')
-        image_as_pixels = asarray(image)
-
+    def extract_faces_from_image(path_to_image, required_size = (160, 160), single_face = False):
+        image_as_pixels = DatasetHelpers.image_from_path_to_pixels_array(path_to_image)
         face_detector = MTCNN()
-        results = face_detector.detect_faces(image_as_pixels)
 
+        results = face_detector.detect_faces(image_as_pixels)
         no_of_faces_to_extract = 1 if single_face else len(results)
         face_array_list = []
         for i in range(no_of_faces_to_extract):
@@ -23,17 +20,18 @@ class DatasetHelpers:
             x2, y2 = x1 + width, y1 + height
             face = image_as_pixels[y1:y2, x1:x2]
             image = Image.fromarray(face)
-            image = image.resize(required_size)
-            face_array = asarray(image)
+            face_array = DatasetHelpers.image_to_pixels_array(image, required_size)
             face_array_list.append(face_array)
 
         return face_array_list
+
 
     @staticmethod
     def extract_faces_from_directory(processed_folder, extraction_folder = None, extracted_face_file_prefix ="face",
                                      face_file_extension =".jpg", single_face = False):
         i = 1
         extracted_faces = []
+
         for filename in listdir(processed_folder):
             path = processed_folder + "/" + filename
             face_array_list = DatasetHelpers.extract_faces_from_image(path, single_face=single_face)
@@ -44,9 +42,14 @@ class DatasetHelpers:
             else:
                 extracted_faces.extend(face_array_list)
 
+        if not extraction_folder: return extracted_faces
+
+
     @staticmethod
-    def extract_faces_from_dataset(dataset_folder, extraction_folder = None, create_extraction_folder = True, single_face = False):
+    def extract_faces_from_dataset(dataset_folder, extraction_folder, create_extraction_folder = True, single_face = False):
         extraction_folder = dataset_folder + "-extracted" if not extraction_folder else extraction_folder
+        extracted_faces = []
+
         if create_extraction_folder: mkdir(extraction_folder)
         for phase in listdir(dataset_folder):
             if create_extraction_folder: mkdir(extraction_folder + "/" + phase)
@@ -55,6 +58,21 @@ class DatasetHelpers:
                 DatasetHelpers.extract_faces_from_directory(dataset_folder + "/" + phase + "/" + subject,
                                                             extraction_folder + "/" + phase + "/" + subject,
                                                             single_face=single_face)
+
+
+    @staticmethod
+    def image_from_path_to_pixels_array(path_to_image, required_size = None):
+        image = Image.open(path_to_image)
+
+        return DatasetHelpers.image_to_pixels_array(image, required_size)
+
+
+    @staticmethod
+    def image_to_pixels_array(image, required_size = None):
+        image = image.convert('RGB')
+        if required_size: image = image.resize(required_size)
+        return asarray(image)
+
 
     @staticmethod
     def load_images(directory):
@@ -65,6 +83,7 @@ class DatasetHelpers:
             image = Image.open(path_to_file)
             images.append(asarray(image))
         return images
+
 
     @staticmethod
     def load_single_dataset(directory):
@@ -79,6 +98,7 @@ class DatasetHelpers:
             output_data.extend(labels)
             i += 1
         return (asarray(input_data), asarray(output_data))
+
 
     @staticmethod
     def load_datasets(datasets_directory):
