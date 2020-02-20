85d8d2e6816b718e97d611206798dd5aa954fae0
Robert Szarvas <robisz98@gmail.com>
2020-02-20 15:08:21 +0200

    Enhancements and fixes for face recognition (#9)
    * add face recognition classifier and helpers
    
    * add predict methods
    
    * interface for saving to binary
    
    * factorize code into helper
    
    * delete unused var
    
    * add persistence of recognition model
    
    * capitalize resource names
    
    * use capitalized resources names
    
    * test file update
    
    * fix path issue
    
    * improve and enhance prediction
    
    * test change

--- Source/FaceRecognition/test_model.py
+++ Source/FaceRecognition/test_model.py
@@ -1,4 +1,5 @@
 from RecognitionModel import RecognitionModel
+from DatasetHelpers import DatasetHelpers
 
 model = RecognitionModel(normalize=True, encode_labels=False)
 model.load_data_from_compressed('locals/5-celebrity-faces-embeddings2.npz', is_embeddings_file=True)
@@ -7,3 +8,4 @@ print(model.test() * 100)
 RecognitionModel.save_model_as_binary(model, "model-pickle")
 model = RecognitionModel.load_model_from_binary("model-pickle")
 print(model.test() * 100)
+print(model.predict_from_faces_images(DatasetHelpers.load_images("locals/Me/val/robi")))
