24bffbbf275feb12698d26d8614001a52f41d42c
Robert Szarvas <robisz98@gmail.com>
2020-04-19 18:02:33 +0300

    use resource localizer for sys config file

--- Source/FaceRecognition/ResourceLocalizer.py
+++ Source/FaceRecognition/ResourceLocalizer.py
@@ -1,7 +1,16 @@
 import configparser
 
 
-class ResourceLocalizer:
+class ResourceLocalizerMeta(type):
+    _instance = None
+
+    def __call__(self):
+        if self._instance is None:
+            self._instance = super().__call__()
+        return self._instance
+
+
+class ResourceLocalizer(metaclass=ResourceLocalizerMeta):
     def __init__(self):
         self.__parser = configparser.ConfigParser()
         self.__parser.read("resources.ini")
@@ -9,3 +18,7 @@ class ResourceLocalizer:
     @property
     def FaceNetModel(self):
         return self.__parser['MODELS']['FACENET_MODEL']
+
+    @property
+    def SystemConfigurationFile(self):
+        return self.__parser['SYSTEM']['CONFIGURATION_FILE']
