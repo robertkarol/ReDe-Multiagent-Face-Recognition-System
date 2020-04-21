98c664a3e9a03304f00be57754e0ab805248da14
Robert Szarvas <robisz98@gmail.com>
2020-04-21 21:19:26 +0300

    added singleton class

--- Source/FaceRecognition/Utils/Singleton.py
+++ Source/FaceRecognition/Utils/Singleton.py
@@ -5,3 +5,18 @@ class SingletonMeta(type):
         if self._instance is None:
             self._instance = super().__call__()
         return self._instance
+
+
+class SingletonPerKey:
+    _instances = {}
+
+    def __init__(self):
+        raise NotImplementedError("Cannot directly create instance. Use factory method instead!")
+
+    def __new__(cls, *args, **kwargs):
+        key = args[0]
+        if key in cls._instances:
+            return cls._instances[key]
+        instance = object.__new__(cls)
+        cls._instances[key] = instance
+        return instance
