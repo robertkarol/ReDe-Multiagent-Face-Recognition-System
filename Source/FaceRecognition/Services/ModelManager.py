142201d70f293245ea397102ef925dbbdfad2a77
Robert Szarvas <robisz98@gmail.com>
2020-04-21 21:19:06 +0300

    directory content versioning

--- Source/FaceRecognition/Services/ModelManager.py
+++ Source/FaceRecognition/Services/ModelManager.py
@@ -1,47 +1,46 @@
 from RecognitionModel import RecognitionModel
-from os import path, listdir, mkdir
-import re
+from Services.DirectoryContentVersioner import DirectoryContentVersioner
+from Utils.Singleton import SingletonPerKey
+from os import path, mkdir
 
 
-class ModelManager:
-    __instances = {}
-
-    def __init__(self):
-        raise NotImplementedError("Cannot directly create instance. Use factory method instead!")
+class ModelManager(SingletonPerKey):
 
     def __new__(cls, *args, **kwargs):
         model_directory = args[0]
-        if model_directory in cls.__instances:
-            return cls.__instances[model_directory]
-        instance = object.__new__(cls)
+        instance = super().__new__(cls, model_directory)
         instance.__models_directory = model_directory
-        instance.__model_version_regex = '.+-v([0-9]+)'
-        instance.__model_filename_regex = '{}-v[0-9]+'
-        instance.__version_suffix = '-v{}'
-        cls.__instances[model_directory] = instance
+        instance.__model_versioner = DirectoryContentVersioner()
         return instance
 
     @classmethod
-    def get_versioning(cls, models_directory: str):
+    def get_manager(cls, models_directory: str):
         return cls.__new__(cls, models_directory)
 
     def publish_model(self, model_basename: str, model: RecognitionModel) -> None:
-        model_dir, models = self.__get_models(model_basename, create_notfound_dir=True)
+        model_dir = self.__get_model_directory(model_basename)
 
-        version = max(models, key=lambda model: model[1])[1] + 1 if len(models) > 0 else 1
-        RecognitionModel.save_model_as_binary(model, path.join(model_dir, self.__version_suffix.format(version)))
+        version = self.__model_versioner.get_latest(model_dir, model_basename)[1] + 1
+        RecognitionModel.save_model_as_binary(model, path.join(model_dir, self.__model_versioner.get_versioned_name(
+            model_basename, version)))
 
     def get_model(self, model_basename: str, version: str = 'latest') -> RecognitionModel:
-        model_dir, models = self.__get_models(model_basename)
+        model_dir = self.__get_model_directory(model_basename)
+
+        if not path.isdir(model_dir):
+            raise LookupError(f"No such {model_dir} model being versioned")
 
         if version == 'latest':
-            model_to_load = max(models, key=lambda model: model[1])[0]
+            model_to_load = self.__model_versioner.get_latest(model_dir, model_basename)[0]
         elif version == 'earliest':
-            model_to_load = min(models, key=lambda model: model[1])[0]
+            model_to_load = self.__model_versioner.get_earliest(model_dir, model_basename)[0]
         else:
-            raise ValueError("Unsupported versioning type")
+            raise ValueError(f"{version} is an unsupported versioning type")
 
         return RecognitionModel.load_model_from_binary(path.join(model_dir, model_to_load))
+
+    def __get_model_directory(self, model_basename):
+        return path.join(self.__models_directory, model_basename)
 
     def __get_models(self, model_basename: str, create_notfound_dir: bool = False):
         model_dir = path.join(self.__models_directory, model_basename)
@@ -52,5 +51,4 @@ class ModelManager:
             else:
                 raise LookupError("No such model being versioned")
 
-        return model_dir, [(model, int(re.search(self.__model_version_regex, model).group(1))) for model in listdir(model_dir)
-                           if re.match(self.__model_filename_regex.format(model_basename), model)]
+        return model_dir, self.__model_versioner.get_versioned_content(model_dir, model_basename)
--- Source/FaceRecognition/Services/NewIdentitiesManager.py
+++ Source/FaceRecognition/Services/NewIdentitiesManager.py
@@ -1,13 +1,49 @@
-from Utils.Singleton import SingletonMeta
+from Services.DirectoryContentVersioner import DirectoryContentVersioner
+from Utils.Singleton import SingletonPerKey
+from os import path, mkdir
+from typing import Iterable, Tuple
 
 
-class NewIdentitiesManager(metaclass=SingletonMeta):
-    def __init__(self, source_locations: list, data_directory):
-        self.__source_locations = source_locations
-        self.__data_directory = data_directory
+# TODO: Make this process-safe
+class NewIdentitiesManager(SingletonPerKey):
 
-    def get_new_identities_dataset_path(self, location: str) -> (int, str):
-        pass
+    def __new__(cls, *args, **kwargs):
+        data_directory = args[0]
+        source_locations = args[1]
+        instance = super().__new__(cls, data_directory)
+        instance.__source_locations = source_locations
+        instance.__data_directory = data_directory
+        instance.__data_versioner = DirectoryContentVersioner()
+        instance.__face_image_filename = "face{}.jpg"
+        return instance
 
-    def publish_identity(self, location: str, name: str, train_data, test_data) -> None:
-        pass
+    @classmethod
+    def get_manager(cls, data_directory: str, source_locations: list):
+        return cls.__new__(cls, data_directory, source_locations)
+
+    def get_newest_identities_dataset_path(self, location: str) -> str:
+        location_dir = self.__get_location_directory(location)
+        data_dir, version = self.__data_versioner.get_latest(location_dir, location)
+        new_data_dir = path.join(location_dir, self.__data_versioner.get_versioned_name(location, version + 1))
+        mkdir(new_data_dir)
+        mkdir(path.join(new_data_dir, 'train'))
+        mkdir(path.join(new_data_dir, 'test'))
+        return path.join(location_dir, data_dir)
+
+    def publish_identity(self, location: str, name: str, train_data: Iterable, test_data: Iterable) -> None:
+        location_dir = self.__get_location_directory(location)
+        data_dir = path.join(location_dir, self.__data_versioner.get_latest(location_dir, location)[0])
+        new_id_train_dir, new_id_test_dir = path.join(data_dir, 'train', name), path.join(data_dir, 'val', name)
+        mkdir(new_id_train_dir)
+        self.__save_images(train_data, new_id_train_dir)
+        mkdir(new_id_test_dir)
+        self.__save_images(test_data, new_id_test_dir)
+
+    def __save_images(self, images, dir):
+        for i, image in enumerate(images):
+            image.save(path.join(dir, self.__face_image_filename.format(i)))
+
+    def __get_location_directory(self, location):
+        if location not in self.__source_locations:
+            raise ValueError(f"{location} is not a valid location")
+        return path.join(self.__data_directory, location)
