4f8c3d70b34ae9b113a4850468455735381a68d5
Robert Szarvas <robisz98@gmail.com>
2020-04-22 18:16:03 +0300

    make NewIdentitiesManager process-safe

--- Source/FaceRecognition/Services/NewIdentitiesManager.py
+++ Source/FaceRecognition/Services/NewIdentitiesManager.py
@@ -1,11 +1,11 @@
 from Services.DirectoryContentVersioner import DirectoryContentVersioner
 from Services.RecognitionLocationsManager import RecognitionLocationsManager
 from Utils.Singleton import SingletonPerKey
+from multiprocessing import Lock
 from os import path, mkdir, listdir
 from typing import Iterable, Tuple
 
 
-# TODO: Make this process-safe
 class NewIdentitiesManager(SingletonPerKey):
 
     def __new__(cls, *args, **kwargs):
@@ -15,6 +15,7 @@ class NewIdentitiesManager(SingletonPerKey):
         instance.__data_directory = data_directory
         instance.__data_versioner = DirectoryContentVersioner()
         instance.__face_image_filename = "face{}.jpg"
+        instance.__lock = Lock()
         return instance
 
     @classmethod
@@ -36,21 +37,29 @@ class NewIdentitiesManager(SingletonPerKey):
             return 0, None
 
         if identities_count > 0:
-            new_data_dir = path.join(location_dir, self.__data_versioner.get_versioned_name(location, version + 1))
-            mkdir(new_data_dir)
-            mkdir(path.join(new_data_dir, 'train'))
-            mkdir(path.join(new_data_dir, 'val'))
+            self.__lock.acquire()
+            try:
+                new_data_dir = path.join(location_dir, self.__data_versioner.get_versioned_name(location, version + 1))
+                mkdir(new_data_dir)
+                mkdir(path.join(new_data_dir, 'train'))
+                mkdir(path.join(new_data_dir, 'val'))
+            finally:
+                self.__lock.release()
 
         return identities_count, path.join(location_dir, data_dir)
 
     def publish_identity(self, location: str, name: str, train_data: Iterable, test_data: Iterable) -> None:
         location_dir = self.__get_location_directory(location)
-        data_dir = path.join(location_dir, self.__data_versioner.get_latest(location_dir, location)[0])
-        new_id_train_dir, new_id_test_dir = path.join(data_dir, 'train', name), path.join(data_dir, 'val', name)
-        mkdir(new_id_train_dir)
-        self.__save_images(train_data, new_id_train_dir)
-        mkdir(new_id_test_dir)
-        self.__save_images(test_data, new_id_test_dir)
+        self.__lock.acquire()
+        try:
+            data_dir = path.join(location_dir, self.__data_versioner.get_latest(location_dir, location)[0])
+            new_id_train_dir, new_id_test_dir = path.join(data_dir, 'train', name), path.join(data_dir, 'val', name)
+            mkdir(new_id_train_dir)
+            self.__save_images(train_data, new_id_train_dir)
+            mkdir(new_id_test_dir)
+            self.__save_images(test_data, new_id_test_dir)
+        finally:
+            self.__lock.release()
 
     def __save_images(self, images, directory):
         for i, image in enumerate(images):
