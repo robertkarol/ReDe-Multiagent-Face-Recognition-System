65862ebc3655c2f671920cc63f3e131a6a1a66d0
Robert Szarvas <robert.szarvas@ni.com>
2020-06-11 16:19:29 +0300

    fix colors

--- Source/FaceRecognition/Utils/Logging.py
+++ Source/FaceRecognition/Utils/Logging.py
@@ -5,19 +5,19 @@ class CustomFormatter(logging.Formatter):
     def __init__(self, fmt=None, datefmt=None, style='%'):
         super().__init__(fmt, datefmt, style)
 
-        grey = "\x1b[38;21m"
-        yellow = "\x1b[33;21m"
-        red = "\x1b[31;21m"
-        bold_red = "\x1b[31;1m"
-        reset = "\x1b[0m"
+        grey = "\x1b[0;37m"
+        yellow = "\x1b[1;33m"
+        red = "\x1b[1;31m"
         light_blue = "\x1b[1;36m"
+        reset = "\x1b[0m"
+        blink_red = "\x1b[5m\x1b[1;31m"
 
         self.__formats = {
             logging.DEBUG: light_blue + self._fmt + reset,
             logging.INFO: grey + self._fmt + reset,
             logging.WARNING: yellow + self._fmt + reset,
             logging.ERROR: red + self._fmt + reset,
-            logging.CRITICAL: bold_red + self._fmt + reset
+            logging.CRITICAL: blink_red + self._fmt + reset
         }
 
     def format(self, record):
